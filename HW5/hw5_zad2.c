8b63020fb1567d524513ca2f4a7f2b5cc0ed1332
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-07-19 01:00:36 +0300

    Update hw5_zad2.c

--- HW5/hw5_zad2.c
+++ HW5/hw5_zad2.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 #include <math.h>
-/*int triangle(double a, double b, double c, double*S, double *P)
-{ 
-   
-    if ()
-    {
-        return -1;
+
+int triangle(double a, double b, double c, double *S, double *P)
+{
+    if (a <=0 || b <= 0 || c <=0){
+     return -1;
     }
-    
+    *P = a + b + c;
+    *S = sqrt(*P*(*P-a)*(*P-a)*(*P-c));
     return 0;
 }
-*/
-int main(){
-   // 1. P = ? , S = ?
+int main() {
+  
    double a, b, c, P, S;
+  
    printf("Enter values for the 3 sides of a triagle:\n");
    while (scanf("%lf %lf %lf", &a, &b, &c) != EOF )
     {
-        if ((a <0 || b < 0 || c < 0))
+        if (triangle(a, b, c, &S, &P) == -1 )
         {
-            printf("Invalid triangle sides!\n");
+             printf("Invalid triangle sides!\n");
         }
         else{
-            P = a + b +c;
-            S = sqrt(P*(P-a)*(P-a)*(P-c));
-    
+            triangle(a, b, c, &S, &P);
             printf("S = %.2lf\n", S);
             printf("P = %.2lf\n", P);
         }
-      // triangle(a, b, c);
     }
-   return 0;
+    return 0;
 }
