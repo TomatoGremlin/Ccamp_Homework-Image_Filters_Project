5862193eb7d442e57f0fdfccc49dcea872396695
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-07-19 00:30:45 +0300

    Update hw5_zad3.c

--- HW5/hw5_zad3.c
+++ HW5/hw5_zad3.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
-/*
-int rectangle(double w, double h, double* S, double *P){
 
-    if (){
+int rectangle(double w, double h, double* S, double *P){
+    if (w <= 0 || h <= 0){
      return -1;
     }
+    *P = 2*(w+h);
+    *S = w * h;
     return 0;
 }
-*/
-int main(){
- // 1. P = ? , S = ?
-   double w, h, P, S;
-   printf("Enter values for height and width of a rectangle:\n");
+int main() {
+  double w, h, P, S;
+  
+  printf("Enter values for height and width of a rectangle:\n");
    while (scanf("%lf %lf", &w, &h) != EOF )
     {
-        if ((w <0 || h < 0 ))
+        if ( rectangle(w, h, &S, &P) == -1 )
         {
             printf("Invalid rectangle sizes!\n");
         }
         else{
-            P = 2*(w+h);
-            S = w * h;
+            rectangle(w, h, &S, &P);
             printf("S = %.2lf\n", S);
             printf("P = %.2lf\n", P);
         }
-      // rectangle(w, h, S, P);
     }
     return 0;
 }
