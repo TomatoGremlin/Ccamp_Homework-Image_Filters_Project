54ec847797ad93357156589baca26d93fe80d01f
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-07-19 01:28:43 +0300

    Update hw5_zad4.c

--- HW5/hw5_zad4.c
+++ HW5/hw5_zad4.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
-
-int main(){
- // 1. P = ? , S = ?
-   double w, h, P, S;
-   printf("Enter values for height and width of a rectangle:\n");
-   while (scanf("%lf %lf", &w, &h) != EOF )
-    {
-        if ((w <0 || h < 0 ))
+#include <math.h>
+int quadEq(double a, double b, double c, double *D, double *x1, double *x2){
+        *D = pow(b,2) - 4*a*c;
+        if (*D < 0)
         {
-            printf("Invalid rectangle sizes!\n");
+            return -1;
+        }
+        *x1 = ((b - 2*b) + sqrt(*D))/(2*a);
+        *x2 = ((b - 2*b) - sqrt(*D))/(2*a);
+    
+        return 0;
+}
+
+int main(void){
+    double a,  b,  c , D, x1,  x2;
+    printf("Enter values for a, b and c for the equation:\n");
+    while (scanf("%lf %lf %lf", &a, &b, &c) != EOF )
+    {
+        D = pow(b,2) - 4*a*c;
+        if ( quadEq( a,  b,  c,  &D, &x1, &x2) == -1)
+        {
+            printf("No real roots\n");
         }
         else{
-            P = 2*(w+h);
-            S = w * h;
-            printf("S = %.2lf\n", S);
-            printf("P = %.2lf\n", P);
+            printf("x1 = %.2lf\n", x1);
+            printf("x2 = %.2lf\n", x2);
         }
-      
     }
-   
     return 0;
 }
