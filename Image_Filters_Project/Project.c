799a9081738ee495c9dedccd5520d84ae70d75c0
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-08-08 02:57:50 +0300

    Command line arguments

--- Image_Filters_Project/Project.c
+++ Image_Filters_Project/Project.c
@@ -110,13 +110,79 @@ void convert_to_grayscale(PPM_Image_Buffer* buf){
   }
 }
 
-int main() {
+int main(int argc, char **argv) {// argc = argument count  ;  argv = array with arguments, [0] = name of the exe
+  
   PPM_Image_Buffer test;
-  PPM_Image_Buffer* buf = &test;
-  read_ppm_color_bitmap("cat.ppm", buf);
+  PPM_Image_Buffer *buf = &test;
+
+  if (argc < 4 || argc > 5)
+  {
+    fprintf(stderr, "Invalid arguments!\n");
+    return -1;
+  }
+
+  // greyscale:
+  if (!strcmp(argv[3], "-g"))
+  {
+   read_ppm_color_bitmap(argv[1], buf);
+   convert_to_grayscale(buf);
+   write_ppm_color_bitmap(argv[2], buf);
+  }
+  // colours:
+  else if (!strcmp(argv[3], "-f"))
+  {
+    if (!strcmp(argv[4], "r"))
+    {
+      //red
+       read_ppm_color_bitmap(argv[1], buf);
+       filter_color_component(buf, 1);
+       write_ppm_color_bitmap(argv[2], buf);
+            
+    }
+    else if (!strcmp(argv[4], "g"))
+    {
+      //green
+       read_ppm_color_bitmap(argv[1], buf);
+       filter_color_component(buf, 2);
+      write_ppm_color_bitmap(argv[2], buf);
+            
+    }
+    else if (!strcmp(argv[4], "b"))
+    {
+      //blue
+       read_ppm_color_bitmap(argv[1], buf);
+       filter_color_component(buf, 4);
+       write_ppm_color_bitmap(argv[2], buf);
+            
+    }
+    else if (!strcmp(argv[4], "rg"))
+    {
+      //red + green
+      read_ppm_color_bitmap(argv[1], buf);
+      filter_color_component(buf, 3);
+      write_ppm_color_bitmap(argv[2], buf);
+            
+    }
+    else if (!strcmp(argv[4], "rb"))
+    {
+      // red + blue 
+      read_ppm_color_bitmap(argv[1], buf);
+      filter_color_component(buf, 5);
+      write_ppm_color_bitmap(argv[2], buf);
+            
+    }
+   else if (!strcmp(argv[4], "gb"))
+   {
+      // green + blue
+      read_ppm_color_bitmap(argv[1], buf);
+      filter_color_component(buf, 6);
+      write_ppm_color_bitmap(argv[2], buf);
+   } 
+  }
 
   // colours:
   /*
+  read_ppm_color_bitmap("cat.ppm", buf);
   filter_color_component(buf, 1);
   write_ppm_color_bitmap("cat1.ppm", buf);
 
@@ -142,7 +208,8 @@ int main() {
   */
 
   // greyscale:
-  /*read_ppm_color_bitmap("cat.ppm", buf);
+  /*
+  read_ppm_color_bitmap("cat.ppm", buf);
   convert_to_grayscale(buf);
   write_ppm_color_bitmap("cat_grey.ppm", buf);
   */
