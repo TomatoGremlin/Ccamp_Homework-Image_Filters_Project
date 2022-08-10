ecf6aac96e1ee6445391c08022bdec0f1e5a95df
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-08-10 22:18:49 +0300

    Update my_filter.h

--- Image_Filters_Project/Code/my_filter.h
+++ Image_Filters_Project/Code/my_filter.h
@@ -1,11 +1,15 @@
 #pragma once
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
-typedef struct Pixel_Data { // RGB info for 1 pixel
-	unsigned red;
-  unsigned green;
-  unsigned blue;
-}Pixel_Data;
+typedef struct Pixel_Data
+{ // RGB info for 1 pixel
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+} Pixel_Data;
 
 typedef struct PPM_Image_Buffer {
 	Pixel_Data* data; // array with pixels
@@ -17,3 +21,6 @@ int write_ppm_color_bitmap(const char *, PPM_Image_Buffer *);
 
 void filter_color_component(PPM_Image_Buffer*, unsigned int);
 void convert_to_grayscale(PPM_Image_Buffer*);
+
+int P3_to_P6(const char *, PPM_Image_Buffer *);
+int P6_to_P3(const char *, PPM_Image_Buffer *);
