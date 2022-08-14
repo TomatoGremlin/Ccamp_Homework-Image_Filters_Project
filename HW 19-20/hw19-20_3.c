12ddd55e7db87a2fbe2ba95916ee3f300a37c011
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-08-15 02:41:55 +0300

    Update hw19-20_3.c

--- HW 19-20/hw19-20_3.c
+++ HW 19-20/hw19-20_3.c
@@ -1,38 +1,54 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <pthread.h>
 
 void *routine1(void* arg){
-    
-    printf("%s\n", arg);
+    char* MSG= (char*)arg;
+    printf("%s\n", MSG);
+    printf("Process PID = %d, Thread ID = %ld\n\n", getpid(), pthread_self());
 }
 
 int main(int argc, char **argp)
 {
     unsigned P = atoi(argp[1]);
     unsigned T = atoi(argp[2]);
-    char* MSG[] = argp[3];
+    char* MSG = argp[3];
     pthread_t threads [T];
 
     for (int i = 0; i < P; i++)
     {
-        for (size_t j = 0; j < T; j++)
-        {
-            if (pthread_create(&threads[j], NULL, routine1, NULL))
+        pid_t process_split = fork();
+        if (-1 == process_split){
+            perror("fork");
+            return(EXIT_FAILURE);
+        }
+
+        if (0 == process_split){
+            // child
+            for (size_t j = 0; j < T; j++)
             {
-                perror("thread create");
-                return EXIT_FAILURE;
+                if (pthread_create(&threads[j], NULL, routine1, argp[3]))
+                {
+                    perror("thread create");
+                    return EXIT_FAILURE;
+                }
             }
+            for (size_t j = 0; j < T; j++)
+            {
+                if (pthread_join(threads[j], NULL))
+                {
+                    perror("join");
+                    return EXIT_FAILURE;
+                }
+            }
+            return 0;
         }
     }
-    for (size_t j = 0; j < T; j++)
-    {
-        if (pthread_join(threads[j], NULL))
-        {
-            perror("join");
-            return EXIT_FAILURE;
-        }
-    }
+    // parent
+    for (int i = 0; i < P; i++) wait(NULL);
+
     return 0;
 }
